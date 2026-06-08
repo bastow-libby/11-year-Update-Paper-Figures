481d54c77fcdd05d8cdae889c6de6f3d1f5445fb
Alex McClure <amcclure@icecube.wisc.edu>
2026-06-08 17:30:11 +0000

    Updating README.md instructions in skymaps folder to better reflect correct lines in code to edit as well as correct directory addresses. Making code running instructions more clear. Adding a directory to produce burnsample data.

--- skymaps/01_l3_processing/submit_root/2015/npx4-execs/rootprod_20150113_45712.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-date
-hostname
-
-/home/amcclure/11-year-Update-Paper-Figures/skymaps/01_l3_processing/l3_i3_to_root_day.py /data/ana/CosmicRay/Anisotropy/IceTop/ITpass2/output/root 2015 0113 /data/ana/CosmicRay/IceTop_level3/exp/IC86.2014_pass2_v01/2015/0113/Run00125908/Level3_IC86.2014_data_Run00125908_Subrun00000000_00000000.i3.bz2 /data/ana/CosmicRay/IceTop_level3/exp/IC86.2014_pass2_v01/2015/0113/Run00125908/Level3_IC86.2014_data_Run00125908_Subrun00000000_00000010.i3.bz2 /data/ana/CosmicRay/IceTop_level3/exp/IC86.2014_pass2_v01/2015/0113/Run00125908/Level3_IC86.2014_data_Run00125908_Subrun00000000_00000020.i3.bz2
-
-date
-echo 'Fin'
