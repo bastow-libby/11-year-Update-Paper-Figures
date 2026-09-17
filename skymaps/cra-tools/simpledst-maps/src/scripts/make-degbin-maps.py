93aae1103fff8095da8665d80aadd313560b807f
Alex McClure <amcclure@icecube.wisc.edu>
2026-09-17 17:15:41 +0000

    fixing typo in make-degbin-maps.py script

--- skymaps/cra-tools/simpledst-maps/src/scripts/make-degbin-maps.py
+++ skymaps/cra-tools/simpledst-maps/src/scripts/make-degbin-maps.py
@@ -101,7 +101,8 @@ if __name__ == "__main__":
     # Lazy approach for all years
     if args.year == ['all']:
         args.year = list(range(2011,2022))
-    if args.year ==['tier1']
+    if args.year ==['tier1']:
+        args.year = list(range(2011, 2015))
 
     # Get working directory for this script
     script_path = Path(__file__).resolve()
